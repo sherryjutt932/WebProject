22a435a182c519d3e2d3ab320e26ba63ba610547
Sheharyar Saeed <85832754+sherryjutt932@users.noreply.github.com>
2022-06-01 22:52:33 +0500

    fix: admin panel is fixed

---
+++ admin/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+import "./responsive.css";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/productScreen";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import OrderScreen from "./screens/OrderScreen";
+import OrderDetailScreen from "./screens/OrderDetailScreen";
+import AddProduct from "./screens/AddProduct";
+import Login from "./screens/LoginScreen";
+import UsersScreen from "./screens/UsersScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
+import NotFound from "./screens/NotFound";
+import PrivateRouter from "./PrivateRouter"
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <PrivateRouter path="/" component={HomeScreen} exact />
+          <PrivateRouter path="/products" component={ProductScreen} />
+          <PrivateRouter path="/category" component={CategoriesScreen} />
+          <PrivateRouter path="/orders" component={OrderScreen} />
+          <PrivateRouter path="/order" component={OrderDetailScreen} />
+          <PrivateRouter path="/addproduct" component={AddProduct} />
+          <PrivateRouter path="/users" component={UsersScreen} />
+          <PrivateRouter path="/product/:id/edit" component={ProductEditScreen} />
+          <Route path="/login" component={Login} />
+          <PrivateRouter path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
